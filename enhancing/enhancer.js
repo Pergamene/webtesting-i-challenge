5f8232f732170d973d2220c691a46c363fb3db4e
Pergamene <smartz373@gmail.com>
2020-04-22 11:33:41 -0600

    methods written

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,45 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const newEnhancement = item.enhancement === 20 ? item.enhancement : item.enhancement + 1;
+  return { 
+    ...item,
+    enhancement: newEnhancement,
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  let newEnhancement = item.enhancement;
+  if (newEnhancement > 16) {
+    newEnhancement--;
+  } else if (newEnhancement > 15) {
+    newEnhancement -= 10;
+  } else {
+    newEnhancement -= 5;
+  }
+  if (newEnhancement < 0) {
+    newEnhancement = 0;
+  }
+  return { 
+    ...item,
+    enhancement: newEnhancement,
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { 
+    ...item,
+    durability: 100,
+  };
 }
 
 function get(item) {
-  return { ...item };
+  let newName = item.name;
+  if (item.durability > 0) {
+    newName = `[+${item.enhancement}]${newName}`;
+  }
+  return { 
+    ...item,
+    name: newName,
+  };
 }
